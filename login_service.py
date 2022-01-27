3f71f27d7f3b5a678208f4babce732157aae9451
Heli Eronen <herojonen@gmail.com>
2022-01-27 22:40:31 +0200

    Try to fix registration

--- login_service.py
+++ login_service.py
@@ -7,7 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 def register(username, password, role):
     hash_value = generate_password_hash(password)
     try:
-        sql = """INSERT INTO users (username, password, role) VALUES (:name, :password, :role)"""
+        sql = "INSERT INTO users (username, password, role) VALUES (:name, :password, :role)"
         db.session.execute(sql, {"name":username, "password":hash_value, "role":role})
         db.session.commit()
     except:
